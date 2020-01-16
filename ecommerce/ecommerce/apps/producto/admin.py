e5aae5c9b3de1a69136707e1f6befb23cb3602e7
Amilcar <amilcar@gmail.com>
2020-01-16 17:08:52 -0600

    Video 4

--- ecommerce/ecommerce/apps/producto/admin.py
+++ ecommerce/ecommerce/apps/producto/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.producto.models import Producto
+admin.site.register(Producto)
--- ecommerce/ecommerce/apps/producto/models.py
+++ ecommerce/ecommerce/apps/producto/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Producto(models.Model):
+	nombre = models.CharField(max_length=40)
+	precio = models.IntegerField()
+	descripcion = models.TextField(null=True)
+	fecha_venta = models.DateField(null=True)
+	def __str__(self):
+		return '{}'.format(self.nombre)
